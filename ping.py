1d1e762ab6b4514b429b349f9f24933d1f5507ae
арбузик <102855641+arbuzic@users.noreply.github.com>
2022-04-16 12:58:47 +0300

    Add files via upload

--- ping.py
+++ ping.py
@@ -6,7 +6,7 @@ win_width = 600
 win_height = 500
 window = display.set_mode((win_width,win_height))
 window.fill(back)
-
+font.init()
 clock = time.Clock()
 FPS = 60
 game = True
@@ -40,20 +40,43 @@ class Player(GameSprite):
         if keys[K_DOWN] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
 
-racket1 = Player('без имени.png', 30, 200,  50, 150, 4)
-racket2 = Player('без имени.png', 520, 200, 50, 150, 4)
-#ball = GameSprite('', 200, 200, 4, 50,50)
+racket1 = Player('без имени.png', 30, 200,  30, 150, 4)
+racket2 = Player('без имени.png', 520, 200, 30, 150, 4)
+ball = GameSprite('мяч.png', 200, 200, 60, 60, 4)
 
+speed_x = 3
+speed_y = 3
+
+font1 = font.Font(None, 35)
+lose1 = font1.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font1.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 while game:
     for e in event.get ():
         if e.type == QUIT:
             game = False
     if finish != True:
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
         window.fill(back)
         racket1.update_l()
         racket2.update_r()
         racket1.reset()
         racket2.reset()
+        ball.reset()
+
+    if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+
+    if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+        speed_x *= -1
+
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose1, (200, 200))
+
+    if ball.rect.x > 600:
+        finish = True
+        window.blit(lose2, (200,200))
 
     clock.tick(FPS)
     display.update()
